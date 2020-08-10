fc9a73e3802f68330aa1553e392391785c09b59f
Kelita Lamar <kelitasukita@gmail.com>
2020-08-11 01:01:43 +0200

    Creates the project structure

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: 'mybills',
+  database: 'mybills',
+  define: {
+    timeStamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
