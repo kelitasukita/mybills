ecc86ac83d2741917dc7856b0f4f39238019666b
Kelita Lamar <kelitasukita@gmail.com>
2020-08-17 12:26:16 +0200

    Register an expense

--- src/routes/expenses.routes.ts
+++ src/routes/expenses.routes.ts
@@ -1,9 +1,36 @@
 import { Router } from 'express';
+import { uuid } from 'uuidv4';
 
 const expensesRouter = Router();
+const expenses = [];
 
 expensesRouter.post('/', (request, response) => {
-  return response.json({ message: 'Hello World' });
+  const {
+    description,
+    value,
+    automaticDebit,
+    dueDate,
+    obs,
+    installments,
+    paid,
+    recurrent,
+  } = request.body;
+
+  const expense = {
+    id: uuid(),
+    description,
+    value,
+    automaticDebit,
+    dueDate,
+    obs,
+    installments,
+    paid,
+    recurrent,
+  };
+
+  expenses.push(expense);
+
+  return response.json(expense);
 });
 
 export default expensesRouter;
