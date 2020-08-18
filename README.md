224649735212f0d26da74eff10ac2ff774929a9e
Kelita Lamar <kelitasukita@gmail.com>
2020-08-18 14:15:59 +0200

    Restarting over

--- README.md
+++
@@ -1,1 +0,0 @@
-# mybills-backend
--- src/routes/index.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import expensesRoutes from './expenses.routes';
-
-const routes = Router();
-
-routes.use('/expenses', expensesRoutes);
-
-export default routes;
