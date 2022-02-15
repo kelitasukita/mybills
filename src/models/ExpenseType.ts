0419f73c31869b11a311f547536a2021e99b7cdd
Kelita Lamar <kelitasukita@gmail.com>
2022-02-15 22:24:12 +0100

    Create type expense model

---
+++ src/models/ExpenseType.ts
@@ -0,0 +1,23 @@
+import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn, OneToOne, JoinColumn } from 'typeorm';
+import CreditCard from './CreditCard';
+
+@Entity('expensestype')
+class ExpenseType {
+  @PrimaryGeneratedColumn('uuid')
+  id: String;
+
+  @Column('varchar')
+  label: string;
+
+  @OneToOne(() => CreditCard)
+  @JoinColumn({ name: 'creditcard_id'})
+  creditcard: CreditCard;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
+
+export default ExpenseType;
