b915ecc648e5d057fcb20af3b00283fb916f18a2
Kelita Lamar <kelitasukita@gmail.com>
2022-02-19 22:51:18 +0100

    Validate credit card fields

--- src/repositories/CreditCardRepository.ts
+++ src/repositories/CreditCardRepository.ts
@@ -11,11 +11,31 @@ interface CreditCardData {
 
 @EntityRepository(CreditCard)
 class CreditCardRepository extends Repository<CreditCard> {
-  public async createCreditCard(data: CreditCardData): Promise<CreditCard> {
+  public async createCreditCard(brand: string, name: string, due_day: bigint, limit: number): Promise<CreditCard> {
 
-    const creditcard = await this.create(data);
+    const creditcard = await this.create({
+      brand,
+      name,
+      due_day,
+      limit
+    });
 
     return this.save(creditcard);
+
+  }
+
+  public async isDuplicated(brand: string, name: string, due_day: bigint, limit: number): Promise<CreditCard | undefined> {
+
+    const creditcard = await this.findOne({
+      where: {
+        brand,
+        name,
+        due_day,
+        limit
+      }
+    });
+
+    return creditcard;
   }
 }
 export default CreditCardRepository;
--- src/services/CreateCreditCardService.ts
+++ src/services/CreateCreditCardService.ts
@@ -1,3 +1,6 @@
+import { max } from 'date-fns';
+import * as Yup from 'yup';
+
 import CreditCard from '../models/CreditCard';
 import CreditCardRepository from '../repositories/CreditCardRepository';
 
@@ -15,11 +18,32 @@ class CreateCreditCardService {
     this.creditcardRepository = repository;
   }
 
-  public async execute(data: CreditCardData): Promise<CreditCard | null> {
-    const creditcard = await this.creditcardRepository.createCreditCard(data);
+  public async execute({ brand, name, due_day, limit }: CreditCardData): Promise<CreditCard> {
+    const schema = Yup.object().shape({
+      brand: Yup.string().max(50).matches(/^\S[aA-zZ ]+$/).required("Brand needs to be informed."),
+      name: Yup.string().max(50).matches(/^\S[aA-zZ ]+$/).required("Name needs to be informed."),
+      due_day: Yup.number().integer().positive().min(1).max(31).required("Insert a valid number."),
+      limit: Yup.number().positive().required("Insert a valid number."),
+    });
+
+    if (!(await schema.isValid({
+      brand,
+      name,
+      due_day,
+      limit
+    }))) {
+
+      throw Error('Validation Failed!');
+    }
+
+    if (await this.creditcardRepository.isDuplicated(brand, name, due_day, limit)) {
+      throw Error('This card already included!')
+    }
+
+    const creditcard = await this.creditcardRepository.createCreditCard(brand, name, due_day, limit);
 
     return creditcard;
   }
-}
+};
 
 export default CreateCreditCardService;
