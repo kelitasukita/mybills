e105df6736c7a5af75721641374fa9d9b25112ab
Kelita Lamar <kelitasukita@gmail.com>
2022-02-20 14:51:13 +0100

    Fix delete funcion for delete all installments

--- src/controllers/Expenses/DeleteExpenseController.ts
+++ src/controllers/Expenses/DeleteExpenseController.ts
@@ -6,13 +6,21 @@ import Expense from "../../models/Expense";
 class DeleteExpenseController {
 
   async handle(request: Request, response: Response) {
+    const repository = getRepository(Expense);
 
     try {
-      const expense = await getRepository(Expense).delete(request.params.id)
+      const { description, value, installments, created_at } = await repository.findOneOrFail(request.params.id);
 
-      return response.json({ message: 'Expense successfully deleted.'});
+      const deleted = await repository.delete({
+        description,
+        value,
+        installments,
+        // created_at / TODO: Resolving how to use date criteria
+      });
 
-    } catch(error: any) {
+      return response.json({ message: 'Expense successfully deleted.' });
+
+    } catch (error: any) {
       return response.status(400).json({ error: error.message });
     }
   }
