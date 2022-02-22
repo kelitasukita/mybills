9955cb830b16638dacac3fd8d12ff18bdb484055
Kelita Lamar <kelitasukita@gmail.com>
2022-02-22 22:44:38 +0100

    Fix query to get unpaid bills

--- src/repositories/ExpensesRepository.ts
+++ src/repositories/ExpensesRepository.ts
@@ -73,7 +73,7 @@ class ExpenseRepository extends Repository<Expense> {
         },
         {
           paid: false,
-          dueDate: LessThan(to)
+          dueDate: LessThan(new Date())
         },
       ],
       order: {
