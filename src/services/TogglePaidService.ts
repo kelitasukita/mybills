eee34916844d39f592446c0a3e222147d980e0c3
Kelita Lamar <kelitasukita@gmail.com>
2022-05-31 14:41:06 +0200

    [ADD] currency conversion, obs with previous value

--- src/services/TogglePaidService.ts
+++ src/services/TogglePaidService.ts
@@ -1,3 +1,4 @@
+import axios from "axios";
 import Expense from "../models/Expense";
 import ExpensesRepository from '../repositories/ExpensesRepository';
 
@@ -13,8 +14,29 @@ class TogglePaidService {
     // Validar se o registro existe no banco findOneOrFail no repository
     const expenseToBeUpdated = await this.repository.findOneOrFail(id);
 
+    let value = expenseToBeUpdated.value;
+    let obs = '';
+
+    if (expenseToBeUpdated.currency !== "EUR" && expenseToBeUpdated.paid == false) { 
+          
+      const response = await axios.post(`https://api.transferwise.com/v3/quotes/`, {
+        "targetAmount": expenseToBeUpdated.value,
+        "sourceCurrency": "EUR", // @todo Mudar isso para o currency padrão do usuário
+        "targetCurrency": expenseToBeUpdated.currency,
+        "preferredPayIn":"BANK_TRANSFER"
+      });
+      obs = `original::${value}`;
+      value = response.data.paymentOptions[0].sourceAmount;
+    }
+
+    if (expenseToBeUpdated.paid == true && expenseToBeUpdated.obs.indexOf('original::') === 0) {
+      value = +expenseToBeUpdated.obs.substring('original::'.length);
+    }
+
     await this.repository.update(id, {
-      paid: !expenseToBeUpdated.paid
+      paid: !expenseToBeUpdated.paid,
+      value,
+      obs
     });
 
     const updatedExpense = await this.repository.findOne(id);
