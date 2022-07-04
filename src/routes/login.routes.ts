5c491b7b4e0363ac67cdc69013ad4717cef2853e
Kelita Lamar <kelitasukita@gmail.com>
2022-07-04 12:56:01 +0200

    Nothing to change

--- src/routes/login.routes.ts
+++ src/routes/login.routes.ts
@@ -3,9 +3,9 @@ import { Request, Response, Router } from "express";
 const loginRouter = Router();
 
 loginRouter.get('/google', async (req: Request, res: Response) => {
-  console.log(req);
+  console.log(req.path);
 
-  return res.json({funciona: 'legal'});
+  return res.json({ funciona: 'legal', query: req.query });
 })
 
 export default loginRouter;
